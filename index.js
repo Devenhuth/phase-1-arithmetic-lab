6f42547d2a59df570dd8535b5edb202943b82725
Deven <devenzachuth@gmail.com>
2022-04-04 15:45:38 -0500

    commit tree 4efbff01380d69eb359b549eec52e1956b1f5130

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let random = Math.floor(Math.random() * 10) + 1; 
+
+let num1 = 2 ;
+let num2 = 31;
+const multiply = num1 * num2;
+
+const max = Math.max(1, 18, 9, 6, 11, 20);
+
+let num3 = 24;
+let num4 = 6;
+const mod = num3 / num4;
